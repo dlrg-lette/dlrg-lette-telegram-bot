87e64fd68241c5e125da92efda53cd4a0bad962f
Darth.Ayokas <simon.ameling@yahoo.de>
2019-09-30 18:25:43 +0200

    SpringBoot Application added

---
+++ src/main/java/org/dlrg/lette/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,13 @@
+package org.dlrg.lette.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotApplication.class, args);
+	}
+
+}
