1c066d0a8eea34e25918da067d2b74700a0d70d3
ayokas <simon.ameling@yahoo.de>
2022-04-15 09:38:04 +0200

    Fixed Healthcheck implementation

--- src/main/java/org/dlrg/lette/telegrambot/misc/Healtcheck.java
+++ src/main/java/org/dlrg/lette/telegrambot/misc/Healtcheck.java
@@ -4,7 +4,9 @@ import org.dlrg.lette.telegrambot.data.ModeratorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Healtcheck {
 
     private static boolean adminBotOK = false;
